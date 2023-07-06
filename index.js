37ee7c46297bb06451bbc8a484bf003a85dac21f
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-07-06 21:04:04 +0100

    setup initial drawing logic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let slider = document.getElementById("grid-size-slider");
 slider.min = 1;
 slider.max = 100;
-slider.value = 64;
+slider.value = 20;
 slider.step = 1;
 let grid = document.getElementById("canvas");
 
@@ -52,29 +52,77 @@ function handleClear(e) {
 		grid.firstChild.remove();
 	}
 }
+
+function handleColour(mode){
+    let colour
+    switch (mode) {
+        case "btn-colour-select":
+            colour = colourPick.value
+            break;
+        case "btn-rainbow":
+            colour = randomColour()
+            break;
+        case "btn-eraser":
+            colour = white
+            break;
+    }
+    return colour
+}
+
+// ...
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+function draw(e) {
+    let pixel = e.target;
+    if (e.type === 'mouseover' && !mouseDown) return
+      let mode = handleButtonSelect(document.querySelector(".selected"));
+      pixel.style.backgroundColor = handleColour(mode);
+    
+  }
+  
+  // Add event listener to each pixel
+  grid.querySelectorAll(".pixel").forEach((pixel) => {
+    pixel.addEventListener("mouseover", draw);
+  });
+  
+  // ...
+  
+
+function makeGrid(){
+    let size = slider.value;
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    
+    for (let i = 0; i < size ** 2; i++) {
+        let tempDiv = document.createElement("div");
+        tempDiv.classList.add("pixel");
+        tempDiv.style.backgroundColor = "white"
+        grid.appendChild(tempDiv);
+    }
+    let radius = "10px "
+    let innerRadius = "7px "
+    grid.style.borderRadius = radius
+    let pixels = document.querySelectorAll(".pixel")
+    pixels[0].style.borderRadius = innerRadius+"0 "+"0 "+"0 "
+    pixels[size-1].style.borderRadius = "0 "+innerRadius+"0 "+"0 "
+    pixels[pixels.length-size].style.borderRadius = "0 "+"0 "+"0 "+innerRadius
+    pixels[pixels.length-1].style.borderRadius = "0 "+"0 "+innerRadius+"0 "
+    grid.querySelectorAll(".pixel").forEach((pixel) => {
+        pixel.addEventListener("mouseover", draw);
+        pixel.addEventListener("mousedown", draw);
+    });
+}
 function handleSlide(e) {
     canvasHeight();
-	let size = slider.value;
-	let sizeString = `${size}x${size}`;
+    let size = slider.value;
     
+    let sizeString = `${size}x${size}`;
 	handleClear();
-	grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-	grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    
-	grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-	let containerWidth = grid.clientWidth;
-	let containerHeight = grid.clientHeight;
-	let smallestDimension = Math.min(containerWidth, containerHeight);
-	let pixelWidth = Math.floor(smallestDimension / size);
-    
-	for (let i = 0; i < size ** 2; i++) {
-        let tempDiv = document.createElement("div");
-		tempDiv.classList.add("pixel");
-		grid.appendChild(tempDiv);
-	}
-    
+    makeGrid()
     handleBorder();
-	handleColourChange();
+	// handleColourChange();
 	document.getElementById("grid-size").innerHTML = sizeString;
 }
 
@@ -83,7 +131,9 @@ function handleBorder() {
 	let isColorSelected = borderBtn.classList.contains("selected");
 	for (let pixel of Array.from(document.getElementsByClassName("pixel"))) {
 		isColorSelected
-			? (pixel.style.border = `1px solid ${invertColor(colourPick.value)}`)
+			? 
+            (pixel.style.border = `1px solid black`)
+            // (pixel.style.border = `1px solid ${invertColor(colourPick.value)}`)
 			: (pixel.style.border = "none");
 	}
 }
@@ -92,17 +142,16 @@ slider.addEventListener("input", handleSlide);
 
 window.addEventListener("DOMContentLoaded", handleSlide);
 
-let clear = document.getElementById("btn-clear");
-clear.onclick = handleClear;
+
 
 let colourPick = document.getElementById("colour-picker");
 
-function handleColourChange() {
-	for (let pixel of Array.from(document.getElementsByClassName("pixel"))) {
-		pixel.style.backgroundColor = colourPick.value;
-	}
-}
-colourPick.addEventListener("input", handleColourChange);
+// function handleColourChange() {
+// 	for (let pixel of Array.from(document.getElementsByClassName("pixel"))) {
+// 		pixel.style.backgroundColor = colourPick.value;
+// 	}
+// }
+// colourPick.addEventListener("input", handleColourChange);
 
 function randomColour() {
 	let rRed = Math.floor(Math.random() * 256);
@@ -112,6 +161,23 @@ function randomColour() {
 }
 
 window.addEventListener("resize", handleSlide);
+
+
+let colourBtn = document.getElementById("btn-colour-select");
+let rainbowBtn = document.getElementById("btn-rainbow");
+let eraserBtn = document.getElementById("btn-eraser");
+let drawButtons = [colourBtn,rainbowBtn,eraserBtn]
+function handleButtonSelect(btn){
+    if(btn.classList.contains("selected")){
+        drawButtons.filter((button)=>{
+            button.id!=btn.id
+        }).forEach((button)=>{
+            button.classList.remove("selected")
+        })
+        return btn.id
+    }
+    
+}
 
 document.querySelectorAll(".btn").forEach((btn) => {
 	btn.addEventListener("click", () => {
@@ -123,15 +189,23 @@ document.querySelectorAll(".btn").forEach((btn) => {
                 handleBorder();
                 break;
             case "btn-colour-select":
+                handleButtonSelect(btn)
+                
                 // handleBorder();
                 break;
             case "btn-rainbow":
+                handleButtonSelect(btn)
                 // handleBorder();
                 break;
             case "btn-eraser":
+                handleButtonSelect(btn)
                 // handleBorder();
                 break;
             case "btn-clear":
+                setTimeout(()=>{btn.classList.remove("selected")},500)
+                for (let pixel of Array.from(document.getElementsByClassName("pixel"))) {
+                    pixel.style.backgroundColor = "white";
+                }
                 // handleBorder();
                 break;
         
