7da18797c065158d7b3e8cfb5920475417102ccb
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-07-06 21:15:32 +0100

    mobile version working

--- index.js
+++ index.js
@@ -53,75 +53,87 @@ function handleClear(e) {
 	}
 }
 
-function handleColour(mode){
-    let colour
-    switch (mode) {
-        case "btn-colour-select":
-            colour = colourPick.value
-            break;
-        case "btn-rainbow":
-            colour = randomColour()
-            break;
-        case "btn-eraser":
-            colour = white
-            break;
-    }
-    return colour
+function handleColour(mode) {
+	let colour;
+	switch (mode) {
+		case "btn-colour-select":
+			colour = colourPick.value;
+			break;
+		case "btn-rainbow":
+			colour = randomColour();
+			break;
+		case "btn-eraser":
+			colour = "white";
+			break;
+	}
+	return colour;
 }
 
 // ...
-let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 function draw(e) {
     let pixel = e.target;
-    if (e.type === 'mouseover' && !mouseDown) return
-      let mode = handleButtonSelect(document.querySelector(".selected"));
-      pixel.style.backgroundColor = handleColour(mode);
-    
+    if (
+      (e.type === "mouseover" && !mouseDown) ||
+      (e.type === "touchmove" && !touchDown)
+    ) {
+      return;
+    }
+    let mode = handleButtonSelect(document.querySelector(".selected"));
+    pixel.style.backgroundColor = handleColour(mode);
   }
   
-  // Add event listener to each pixel
+  // Add event listeners to each pixel
   grid.querySelectorAll(".pixel").forEach((pixel) => {
     pixel.addEventListener("mouseover", draw);
+    pixel.addEventListener("touchmove", draw);
   });
   
-  // ...
+  // Add touch event listeners to the entire grid
+  grid.addEventListener("touchstart", () => {
+    touchDown = true;
+  });
   
+  grid.addEventListener("touchend", () => {
+    touchDown = false;
+  });
+  
+function makeGrid() {
+	let size = slider.value;
+	grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-function makeGrid(){
-    let size = slider.value;
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    
-    
-    for (let i = 0; i < size ** 2; i++) {
-        let tempDiv = document.createElement("div");
-        tempDiv.classList.add("pixel");
-        tempDiv.style.backgroundColor = "white"
-        grid.appendChild(tempDiv);
-    }
-    let radius = "10px "
-    let innerRadius = "7px "
-    grid.style.borderRadius = radius
-    let pixels = document.querySelectorAll(".pixel")
-    pixels[0].style.borderRadius = innerRadius+"0 "+"0 "+"0 "
-    pixels[size-1].style.borderRadius = "0 "+innerRadius+"0 "+"0 "
-    pixels[pixels.length-size].style.borderRadius = "0 "+"0 "+"0 "+innerRadius
-    pixels[pixels.length-1].style.borderRadius = "0 "+"0 "+innerRadius+"0 "
-    grid.querySelectorAll(".pixel").forEach((pixel) => {
-        pixel.addEventListener("mouseover", draw);
-        pixel.addEventListener("mousedown", draw);
-    });
+	for (let i = 0; i < size ** 2; i++) {
+		let tempDiv = document.createElement("div");
+		tempDiv.classList.add("pixel");
+		tempDiv.style.backgroundColor = "white";
+		grid.appendChild(tempDiv);
+	}
+	let radius = "10px ";
+	let innerRadius = "7px ";
+	grid.style.borderRadius = radius;
+	let pixels = document.querySelectorAll(".pixel");
+	pixels[0].style.borderRadius = innerRadius + "0 " + "0 " + "0 ";
+	pixels[size - 1].style.borderRadius = "0 " + innerRadius + "0 " + "0 ";
+	pixels[pixels.length - size].style.borderRadius =
+		"0 " + "0 " + "0 " + innerRadius;
+	pixels[pixels.length - 1].style.borderRadius =
+		"0 " + "0 " + innerRadius + "0 ";
+	grid.querySelectorAll(".pixel").forEach((pixel) => {
+		pixel.addEventListener("mouseover", draw);
+		pixel.addEventListener("mousedown", draw);
+	});
 }
 function handleSlide(e) {
-    canvasHeight();
-    let size = slider.value;
-    
-    let sizeString = `${size}x${size}`;
+	canvasHeight();
+	let size = slider.value;
+
+	let sizeString = `${size}x${size}`;
 	handleClear();
-    makeGrid()
-    handleBorder();
+	makeGrid();
+	handleBorder();
 	// handleColourChange();
 	document.getElementById("grid-size").innerHTML = sizeString;
 }
@@ -131,18 +143,15 @@ function handleBorder() {
 	let isColorSelected = borderBtn.classList.contains("selected");
 	for (let pixel of Array.from(document.getElementsByClassName("pixel"))) {
 		isColorSelected
-			? 
-            (pixel.style.border = `1px solid black`)
-            // (pixel.style.border = `1px solid ${invertColor(colourPick.value)}`)
-			: (pixel.style.border = "none");
+			? (pixel.style.border = `1px solid black`)
+			: // (pixel.style.border = `1px solid ${invertColor(colourPick.value)}`)
+			  (pixel.style.border = "none");
 	}
 }
 
 slider.addEventListener("input", handleSlide);
 
 window.addEventListener("DOMContentLoaded", handleSlide);
-
-
 
 let colourPick = document.getElementById("colour-picker");
 
@@ -162,55 +171,48 @@ function randomColour() {
 
 window.addEventListener("resize", handleSlide);
 
-
 let colourBtn = document.getElementById("btn-colour-select");
 let rainbowBtn = document.getElementById("btn-rainbow");
 let eraserBtn = document.getElementById("btn-eraser");
-let drawButtons = [colourBtn,rainbowBtn,eraserBtn]
-function handleButtonSelect(btn){
-    if(btn.classList.contains("selected")){
-        drawButtons.filter((button)=>{
-            button.id!=btn.id
-        }).forEach((button)=>{
-            button.classList.remove("selected")
-        })
-        return btn.id
-    }
-    
+let drawButtons = [colourBtn, rainbowBtn, eraserBtn];
+
+function handleButtonSelect(btn) {
+	if (btn.classList.contains("selected")) {
+		drawButtons
+			.filter((button) => button !== btn)
+			.forEach((button) => {
+				button.classList.remove("selected");
+			});
+		return btn.id;
+	}
 }
 
 document.querySelectorAll(".btn").forEach((btn) => {
 	btn.addEventListener("click", () => {
-        btn.classList.contains("selected")
-        ? btn.classList.remove("selected")
-        : btn.classList.add("selected");
-        switch (btn.id) {
-            case "btn-border":
-                handleBorder();
-                break;
-            case "btn-colour-select":
-                handleButtonSelect(btn)
-                
-                // handleBorder();
-                break;
-            case "btn-rainbow":
-                handleButtonSelect(btn)
-                // handleBorder();
-                break;
-            case "btn-eraser":
-                handleButtonSelect(btn)
-                // handleBorder();
-                break;
-            case "btn-clear":
-                setTimeout(()=>{btn.classList.remove("selected")},500)
-                for (let pixel of Array.from(document.getElementsByClassName("pixel"))) {
-                    pixel.style.backgroundColor = "white";
-                }
-                // handleBorder();
-                break;
-        
-            default:
-                break;
-        }
+		btn.classList.contains("selected")
+			? btn.classList.remove("selected")
+			: btn.classList.add("selected");
+		switch (btn.id) {
+			case "btn-border":
+				handleBorder();
+				break;
+			case "btn-colour-select":
+			case "btn-rainbow":
+			case "btn-eraser":
+				handleButtonSelect(btn);
+				break;
+			case "btn-clear":
+				setTimeout(() => {
+					btn.classList.remove("selected");
+				}, 500);
+				for (let pixel of Array.from(
+					document.getElementsByClassName("pixel")
+				)) {
+					pixel.style.backgroundColor = "white";
+				}
+				break;
+			default:
+				break;
+		}
 	});
 });
